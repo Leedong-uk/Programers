946aa49fe1b18e5f8bad7b5ce330f121d80433eb
Leedong-uk <du123kim@naver.com>
2021-07-16 16:55:57 +0900

    백준2577번

--- pyhon.py
+++ pyhon.py
@@ -1,9 +1,19 @@
-arr = []
-print("평균 내고자 하는 수를 입력해주세요 ,로끊어서")
-arr = input().split(",")
-print(arr)
-n=len(arr)
+num1=int(input())
+num2=int(input())
+num3=int(input())
+
+cal=num1 * num2 * num3
+
+cal2=str(cal)
+cal3=list(cal2)
+
+n=len(cal3)
+
 for i in range(0,n):
-    arr[i] = int(arr[i])
-print(arr)
-print(n)    
+    cal3[i]=int(cal3[i])
+
+for i in range (0 ,10):
+    a=cal3.count(i)
+    print(a)
+    
+
