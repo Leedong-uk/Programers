1148d4e5da2275da01a51f4de29d5eefce86d715
Leedong-uk <du123kim@naver.com>
2021-07-16 16:11:40 +0900

    백준 10818번

---
+++ pratice.py
@@ -0,0 +1,19 @@
+print("정수의 개수를 입력하세요")
+n=int(input())
+
+arr= []
+print('정수를 입력하세요')
+arr = input().split()
+
+for i in range(0,n):
+    arr[i] = int(arr[i])
+
+Max = -1000000
+Min = 1000000
+for i in arr:
+    if i > Max:
+        Max=i
+    if i < Min :
+        Min=i
+
+print(Min, Max) 
--- pyhon.py
+++ pyhon.py
@@ -1,3 +1,9 @@
-print('hello world')
-print('안녕')
-print("하세요")
+arr = []
+print("평균 내고자 하는 수를 입력해주세요 ,로끊어서")
+arr = input().split(",")
+print(arr)
+n=len(arr)
+for i in range(0,n):
+    arr[i] = int(arr[i])
+print(arr)
+print(n)    
